c44f45d33d838de5db7a86c1c25ae5de46386c4b
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-09-05 16:20:02 -0500

    create the project and some first logic selected number

---
+++ components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Card = (props) => {
+  return (
+    <View style={{...styles.card, ...props.style}}>
+      {props.children}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black', // shadow only support by ios
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    elevation: 5, // only for android
+    shadowOpacity: 0.26,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  }
+})
+
+export default Card
